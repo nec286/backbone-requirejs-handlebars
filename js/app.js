c145b7306db14776fc87d71fffc28ae06cff2abc
Peter Ludlow <peter.t.ludlow@gmail.com>
2012-08-04 11:13:44 -0400

    main.js bootstrap / loading core dependencies

---
+++ js/app.js
@@ -0,0 +1,9 @@
+define(function(require) {
+   
+   require('jquery'),
+   require('underscore'),
+   require('backbone');
+  
+   console.log('$, _, Backbone... rock on!!!');
+
+})
---
+++ js/common.js
@@ -0,0 +1,6 @@
+require.config({
+   baseUrl: 'js/libs',
+   paths: {
+      app: '../app',
+   }
+});
